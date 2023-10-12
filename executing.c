d067c71ea2307537b37860ce3e3624584df16b90
Yumvagusenga <yumvagusengajaphet@gmail.com>
2023-10-12 03:14:25 -0700

    executing function

---
+++ executing.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+/**
+ * execute_cmd- executing function
+ * child_pid: child process id
+ * @cmd: string name
+ */
+void execute_cmd(const char *cmd)
+{
+	pid_t child_pid = fork();
+
+	if (child_pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (child_pid == 0)
+	{
+		char *args[] = {cmd, NULL};
+		char *envp[] = {NULL};
+
+		if (execve(cmd, args, envp) == -1)
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		wait(NULL);
+	}
+}
