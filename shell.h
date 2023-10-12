7afc4576caebfc98de5c98e993072db9723432b2
Yumvagusenga <yumvagusengajaphet@gmail.com>
2023-10-12 03:50:53 -0700

    user input

--- shell.h
+++ shell.h
@@ -5,10 +5,13 @@
 #include <string.h>
 #include <unistd.h>
 #include <sdtbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-void j_print(const char *printing)
-void display(void)
-
+void j_print(const char *printing);
+void display(void);
+void execute_cmd(const char *cmd);
+void read_cmd(char *cmd, size_t size);
 
 
 
